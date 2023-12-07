4452495b9b55c04c4ac0bff268b744cdf17b0e0c
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-12-07 15:00:46 -0500

    removal

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+4!%2!$f1v3dq$pk0e3pzyl3w5hw2x6oo!4lansq+d^mvmbkg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
