c4c1529dea0320ab706fe130bbe5f3ca54148775
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-12-20 12:17:19 -0500

    extenalized and Auth app

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig',
+    'Auth.apps.AuthConfig',
 
 
     'rest_framework',
